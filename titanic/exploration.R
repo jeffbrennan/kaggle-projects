086a93b83c4b676daddb8fdefc470f11bc3d92ff
Jeff Brennan <jeffbrennan10@gmail.com>
2018-10-02 17:17:03 -0500

    fix title function add exploratory graphs

--- titanic/exploration.R
+++ titanic/exploration.R
@@ -32,7 +32,7 @@ ggplot(train, aes(x = Pclass, fill = factor(Survived))) +
     ylab("Total Count") +
     labs(fill = "Survived")
 
-## unique names
+## unique Names
 length(unique(as.character(data.combined$Name)))
 
 ##miss and mr.
@@ -51,17 +51,17 @@ mrses[1:15,]
 males = data.combined[which(train$Sex == "male"),]
 males[1:15,]
 
-## title utility function | needs fixing
-extractTitle = function(name){
-    name = as.character(name)
+## title utility function |
+extractTitle = function(Name){
+    Name = as.character(Name)
     
-    if (length(grep("Miss.", name)) > 0) {
+    if (length(grep("Miss.", Name)) > 0) {
         return ("Miss.")
-    } else if (length(grep("Master.", name)) > 0){
+    } else if (length(grep("Master.", Name)) > 0){
         return ("Master.")
-    } else if (length(grep("Mrs.", name)) > 0){
+    } else if (length(grep("Mrs.", Name)) > 0){
         return("Mrs.")
-    } else if (length(grep("Mr.", name)) > 0){
+    } else if (length(grep("Mr.", Name)) > 0){
         return ("Mr.")
     } else {
         return ("Other")
@@ -70,12 +70,13 @@ extractTitle = function(name){
     
 titles = NULL
 for (i in 1:nrow(data.combined)) {
-    titles = c(titles, extractTitle(data.combined[i, "name"]))
+    titles = c(titles, extractTitle(data.combined[i, "Name"]))
 }
 data.combined$title = as.factor(titles)
 
 
-## visualize survived 
+## visualize survived by title and class
+## males don't survive well except for Masters. 
 ggplot(data.combined[1:891,], aes(x = title, fill = Survived)) +
     geom_bar(width = 0.5) + 
     facet_wrap(~Pclass) +
@@ -84,13 +85,98 @@ ggplot(data.combined[1:891,], aes(x = title, fill = Survived)) +
     ylab("Total Count") +
     labs(fill = "Survived")
 
+## distro of males and females 
+table(data.combined$Sex)
 
 
 
+## visualize sex, pclass, survival
+## females always surive better, everyone dies in 3rd class
+## exploratory plot (lacks Master / Miss / Mr to separate by children, married etc)
+ggplot(data.combined[1:891,], aes(x = Sex, fill = Survived)) +
+    geom_bar(width = 0.5) + 
+    facet_wrap(~Pclass) +
+    ggtitle("Pclass") +
+    xlab("Sex") +
+    ylab("Total Count") +
+    labs(fill = "Survived")
 
 
 
+## age summary
+## lots of null ages (263), most of which are in training data (177)
+## options to deal with
+    ## 1. Replace N/A with median / mean values of data
+    ## 2. Create predictive model to determine what value of missing data should be
+    ## 3. Find proxy to estimate age (Title)
+summary(data.combined$Age)
+summary(data.combined[1:891, "Age"])
 
+## more evidence for women/children first theory
+ggplot(data.combined[1:891,], aes(x = Age, fill = Survived)) +
+    facet_wrap(~Sex + Pclass) +
+    geom_histogram(binwidth = 10) + 
+    xlab("Age") +
+    ylab("Total Count")
+
+
+## investigate Master title
+## found to be reasonable proxy for children (median 4 | low number of missing)
+boys = data.combined[which(data.combined$title=='Master.'),]
+summary(boys$Age)
+
+
+## investigate misses
+misses = data.combined[which(data.combined$title=='Miss.'),]
+summary(misses$Age)
+
+ggplot(misses[misses$Survived != "None",], aes(x = Age, fill = Survived)) +
+    facet_wrap(~Pclass) +
+    geom_histogram(binwidth = 5) + 
+    ggtitle("Age for 'Miss.' by Pclass")
+    xlab("Age") +
+    ylab("Total Count")
+
+    
+    
+## female children chould have different survival rate
+    
+misses.alone = misses[which(misses$SibSp == 0 & misses$Parch == 0),]
+summary(misses.alone$Age)
+length(which(misses.alone$Age <= 14.5))
+
+
+## explore sibsp variable
+summary(data.combined$SibSp)
+
+## can it be treated as a factor
+length(unique(data.combined$SibSp))
+
+
+## create family size feature 
+temp.Sibsp = c(train$SibSp, test$SibSp)
+temp.parch = c(train$Parch, test$Parch)
+data.combined$family.size = as.factor(temp.Sibsp + temp.parch + 1)
+
+
+## visualize family size
+
+ggplot(data.combined[1:891,], aes(x = family.size, fill = Survived)) +
+    facet_wrap(~Pclass + title) +
+    geom_bar(width = 1) + 
+    xlab("Family Size") +
+    ylab("Total Count") +
+    ylim(0,300) +
+    labs(fill = "Survived")
+
+
+## ticket
+
+## fare
+
+## cabin
+
+## embarked
 
 
 
