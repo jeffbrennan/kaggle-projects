1b2be23b102605c60012436175d74e356f8bd58f
Jeff Brennan <jeffbrennan10@gmail.com>
2018-10-02 01:57:07 -0500

    add analysis by title

---
+++ movie-database/movie-exploration.r
@@ -0,0 +1,1 @@
+## initial movie commit
--- titanic/exploration.R
+++ titanic/exploration.R
@@ -26,4 +26,82 @@ library(ggplot2)
 
 ## hypothesis - rich survived disproportionately 
 train$Pclass = as.factor(train$Pclass)
+ggplot(train, aes(x = Pclass, fill = factor(Survived))) +
+    geom_bar(width = 0.5) + 
+    xlab("Pclass") +
+    ylab("Total Count") +
+    labs(fill = "Survived")
+
+## unique names
+length(unique(as.character(data.combined$Name)))
+
+##miss and mr.
+library(stringr)
+
+
+##gets all miss from datasets and store in misses variable
+misses = data.combined[which(str_detect(data.combined$Name, "Miss.")),]
+misses[1:15,]
+    
+## gets mrs.
+mrses = data.combined[which(str_detect(data.combined$Name, "Mrs.")),]
+mrses[1:15,]
+
+## males
+males = data.combined[which(train$Sex == "male"),]
+males[1:15,]
+
+## title utility function | needs fixing
+extractTitle = function(name){
+    name = as.character(name)
+    
+    if (length(grep("Miss.", name)) > 0) {
+        return ("Miss.")
+    } else if (length(grep("Master.", name)) > 0){
+        return ("Master.")
+    } else if (length(grep("Mrs.", name)) > 0){
+        return("Mrs.")
+    } else if (length(grep("Mr.", name)) > 0){
+        return ("Mr.")
+    } else {
+        return ("Other")
+    }
+}
+    
+titles = NULL
+for (i in 1:nrow(data.combined)) {
+    titles = c(titles, extractTitle(data.combined[i, "name"]))
+}
+data.combined$title = as.factor(titles)
+
+
+## visualize survived 
+ggplot(data.combined[1:891,], aes(x = title, fill = Survived)) +
+    geom_bar(width = 0.5) + 
+    facet_wrap(~Pclass) +
+    ggtitle("Pclass") +
+    xlab("Title") +
+    ylab("Total Count") +
+    labs(fill = "Survived")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
